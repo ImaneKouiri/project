7d425d12f08165d09934a6b39a2bcd5542472d6e
HafssaArchi <hafssaassou04@gmail.com>
2026-01-04 19:45:25 +0100

    Refactor handleEmprunter method to create Livre

--- src/main/java/com/bibliotheque/controller/EmpruntController.java
+++ src/main/java/com/bibliotheque/controller/EmpruntController.java
@@ -109,25 +109,14 @@ public class EmpruntController {
                                 "Limite maximale : 3 emprunts simultanés");
                 return;
             }
-<<<<<<< HEAD
 
             // Livre livre = livreService.findByIsbn(isbn);
-            Livre livre = new Livre();
-=======
-            
-            Livre livre = new Livre(null, null, null, 0);
->>>>>>> bf43f1ae8d515c29ef20ba26de27d258b8113c7b
+            Livre livre = new Livre(membreIdText, membreIdText, membreIdText, membreId);
             livre.setIsbn(isbn);
             livre.setDisponible(true);
 
             Membre membre = new Membre();
             membre.setId(membreId);
-<<<<<<< HEAD
-
-=======
-            
-            
->>>>>>> bf43f1ae8d515c29ef20ba26de27d258b8113c7b
             empruntService.emprunterLivre(livre, membre);
 
             LocalDate dateRetour = LocalDate.now().plusWeeks(14);
