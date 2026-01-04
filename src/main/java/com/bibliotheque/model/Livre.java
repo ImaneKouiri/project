5283599d8028c05a9a8c98f6cbc9dcdd019a8020
kawtarelbay <elbaykawtar@gmail.com>
2026-01-04 16:32:04 +0100

    Update Livre.java

--- src/main/java/com/bibliotheque/model/Livre.java
+++ src/main/java/com/bibliotheque/model/Livre.java
@@ -1,32 +1,92 @@
 package com.bibliotheque.model;
 
-public class Livre {
-    public String Titre;
-    public String Isbn;
-    public boolean Disponible;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.BooleanProperty;
 
-    public String getTitre() {
-        return Titre;
+public class Livre extends Document implements Empruntable {
+    private String auteur;
+    private int anneePublication;
+    private boolean disponible;
+    private String isbn;
+
+    // Propriétés JavaFX pour TableView
+    private final StringProperty isbnProperty;
+    private final StringProperty titreProperty;
+    private final StringProperty auteurProperty;
+    private final IntegerProperty anneePublicationProperty;
+    private final BooleanProperty disponibleProperty;
+
+    public Livre(String isbn, String titre, String auteur, int anneePublication) {
+        super(isbn, titre);
+        this.isbn = isbn;
+        this.auteur = auteur;
+        this.anneePublication = anneePublication;
+        this.disponible = true;
+        this.isbnProperty = new SimpleStringProperty(isbn);
+        this.titreProperty = new SimpleStringProperty(titre);
+        this.auteurProperty = new SimpleStringProperty(auteur);
+        this.anneePublicationProperty = new SimpleIntegerProperty(anneePublication);
+        this.disponibleProperty = new SimpleBooleanProperty(true);
     }
 
-    public void setTitre(String Titre) {
-        this.Titre = Titre;
+    public String getAuteur() { return auteur; }
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+        this.auteurProperty.set(auteur);
     }
 
-    public String getIsbn() {
-        return Isbn;
+    public int getAnneePublication() { return anneePublication; }
+    public void setAnneePublication(int anneePublication) {
+        this.anneePublication = anneePublication;
+        this.anneePublicationProperty.set(anneePublication);
     }
 
-    public void setIsbn(String isbn) {
-        this.Isbn = isbn;
-    }
-
-    public boolean isDisponible() {
-        return Disponible;
-    }
-
+    public boolean isDisponible() { return disponible; }
     public void setDisponible(boolean disponible) {
-        this.Disponible = disponible;
+        this.disponible = disponible;
+        this.disponibleProperty.set(disponible);
     }
 
+    public String getIsbn() { return isbn; }
+
+    // Propriétés JavaFX
+    public StringProperty isbnProperty() { return isbnProperty; }
+    public StringProperty titreProperty() { return titreProperty; }
+    public StringProperty auteurProperty() { return auteurProperty; }
+    public IntegerProperty anneePublicationProperty() { return anneePublicationProperty; }
+    public BooleanProperty disponibleProperty() { return disponibleProperty; }
+
+    @Override
+    public double calculerPenaliteRetard(int joursRetard) {
+        return joursRetard * 0.50;
+    }
+
+    @Override
+    public boolean peutEtreEmprunte() {
+        return disponible;
+    }
+
+    @Override
+    public void emprunter() {
+        if (!disponible) {
+            throw new IllegalStateException("Livre déjà emprunté");
+        }
+        this.disponible = false;
+        this.disponibleProperty.set(false);
+    }
+
+    @Override
+    public void retourner() {
+        this.disponible = true;
+        this.disponibleProperty.set(true);
+    }
+
+    @Override
+    public String toString() {
+        return titre + " (" + auteur + ", " + anneePublication + ")";
+    }
 }
