d052ea3da65beed60751e5bec32b047fd36a0a2c
kawtarelbay <elbaykawtar@gmail.com>
2026-01-04 16:30:37 +0100

    Update LivreDAO.java

--- src/main/java/com/bibliotheque/dao/LivreDAO.java
+++ src/main/java/com/bibliotheque/dao/LivreDAO.java
@@ -2,23 +2,16 @@ package com.bibliotheque.dao;
 
 import com.bibliotheque.model.Livre;
 import java.util.List;
+import java.sql.SQLException;
 
 public interface LivreDAO {
-    void save(Livre livre);
-
-    Livre findById(String id);
-
-    List<Livre> findAll();
-
-    void update(Livre livre);
-
-    void delete(String id);
-
-    List<Livre> findByTitre(String titre);
-
-    List<Livre> findByAuteur(String auteur);
-
-    Livre findByIsbn(String isbn);
-
-    List<Livre> findDisponibles();
+    void save(Livre livre) throws SQLException;
+    Livre findById(String isbn) throws SQLException;
+    List<Livre> findAll() throws SQLException;
+    void update(Livre livre) throws SQLException;
+    void delete(String isbn) throws SQLException;
+    List<Livre> findByAuteur(String auteur) throws SQLException;
+    List<Livre> findByTitre(String titre) throws SQLException;
+    List<Livre> findDisponibles() throws SQLException;
+    List<Livre> findByTitreOuAuteur(String recherche) throws SQLException;
 }
