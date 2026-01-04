d9b6a33ef88d88da5497993ba01611ede9791aa9
ImaneKouiri <kouir.imane2005@gmail.com>
2026-01-04 11:50:36 +0100

    Finalisation de l’infrastructure et de la configuration principale de l’application

--- src/main/java/com/bibliotheque/controller/MainController.java
+++ src/main/java/com/bibliotheque/controller/MainController.java
@@ -1,16 +1,58 @@
 package com.bibliotheque.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.TabPane;
+import javafx.scene.layout.VBox;
 
 public class MainController {
 
   @FXML
-  private TabPane tabPane;
+  private VBox menuPane;
+
+  @FXML
+  private VBox livresPane;
+
+  @FXML
+  private VBox membresPane;
+
+  @FXML
+  private VBox empruntsPane;
 
   @FXML
   public void initialize() {
-    // Appelé automatiquement au chargement du FXML
+    // Au démarrage, on affiche seulement le menu
+    hideAll();
+    menuPane.setVisible(true);
     System.out.println("MainController chargé avec succès");
+  }
+
+  private void hideAll() {
+    menuPane.setVisible(false);
+    livresPane.setVisible(false);
+    membresPane.setVisible(false);
+    empruntsPane.setVisible(false);
+  }
+
+  @FXML
+  private void showLivres() {
+    hideAll();
+    livresPane.setVisible(true);
+  }
+
+  @FXML
+  private void showMembres() {
+    hideAll();
+    membresPane.setVisible(true);
+  }
+
+  @FXML
+  private void showEmprunts() {
+    hideAll();
+    empruntsPane.setVisible(true);
+  }
+
+  @FXML
+  private void backToMenu() {
+    hideAll();
+    menuPane.setVisible(true);
   }
 }
